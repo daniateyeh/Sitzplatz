d4946d92398d2d223398a4e16f9a2513b4553078
daniateyeh <58683578+daniateyeh@users.noreply.github.com>
2022-10-06 14:42:58 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# erste version einer App
+import kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class HelloApp(App):
+    def build(self):
+        return Label(text='Hello World')
+
+
+if __name__ == "__main__":
+    HelloApp().run()
+
+
+
